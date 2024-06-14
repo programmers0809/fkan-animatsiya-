578c3722daf19ed6be5827444686d108f9bc3376
feruz abdullyev <165376645+programmers0809@users.noreply.github.com>
2024-06-14 15:02:34 +0500

    Add files via upload

---
+++ mani.js
@@ -0,0 +1,25 @@
+// 1
+// if (x > y) {
+//     console.log(y); // This will print the value of y if x is greater than y
+// }
+// 2
+// let a=4;
+// let b=5;
+// let c=7;
+// if
+// let sonlar=[3,2,,5,6,7,5,3,]
+// let sonlar = [3, 2, 5, 6, 7, 5, 3];
+
+// // Function to find even numbers in the array
+// function findEvenNumbers(arr) {
+//     let evenNumbers = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 === 0) {
+//             evenNumbers.push(arr[i]);
+//         }
+//     }
+//     return evenNumbers;
+// }
+
+// let evenNumbers = findEvenNumbers(sonlar);
+// console.log("Even numbers:", evenNumbers);
